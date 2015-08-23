a1133f33c0dc3e7b0a77fc34972c6969263386b4
chuqiankcq <chuqian.koh.2014@sis.smu.edu.sg>
2015-08-23 16:03:13 +0800

    Add chuanfa website

---
+++ chuanfa-website draft/website/lite_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$email_to = "enquiry@c2sec.com.sg"; // your email address
+$email_subject = "Contact Form Message"; // email subject line
+$thankyou = "formsubmitted.html"; // thank you page
+
+?>
